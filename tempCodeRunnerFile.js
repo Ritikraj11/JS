8b8bcace7ad1c40561a0310bbabce3cc5dcd51c8
Ritikraj11 <160333960+Ritikraj11@users.noreply.github.com>
2025-01-06 19:13:12 +0530

    deleted an unusual tempCodeRunnerFile

--- tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const name = "Ritik"
-
-// console.log(name.at(2));
-// console.log(name.charAt(3));
-// console.log(name.charCodeAt(3))  // it returns the unicode of the character
-// console.log(name[2]);
